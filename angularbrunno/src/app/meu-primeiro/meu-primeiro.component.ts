08db388c6945c763f96999ca409aa8978c6230bf
brunnoterada <brunoterada99@gmail.com>
2021-06-08 16:55:43 -0300

    inicio angular

---
+++ angularbrunno/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'meu-primeiro-component',
+    template:`
+    <p>
+    My fisrt component 
+    </p>
+    `
+})
+export class MeuPrimeiroComponent {}
